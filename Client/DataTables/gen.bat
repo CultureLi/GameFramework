886edf8a25aa7c461f0d54a78de93262ac13193b
liwenhua <wenhua.li@igg.com>
2025-06-11 21:57:21 +0800

    Framework Config

---
+++ Client/DataTables/gen.bat
@@ -0,0 +1,14 @@
+set WORKSPACE=..
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=.
+
+dotnet %LUBAN_DLL% ^
+    -t client ^
+    -c cs-bin ^
+    -d bin ^
+    --conf %CONF_ROOT%\luban.conf ^
+    -x outputCodeDir=%WORKSPACE%\Assets\HotFixScripts\GameMain\Config\Gen ^
+    -x outputDataDir=%WORKSPACE%\Assets\BundleRes\Config ^
+    -x pathValidator.rootDir=%WORKSPACE%\Client
+
+pause
